98a5e3b6f8684dbadd0c0b48a1ba214179a32377
lauralardies <lrodrlar@myuax.com>
2022-03-27 11:25:05 +0200

    comentarios

--- excepciones.py
+++ excepciones.py
@@ -3,11 +3,11 @@ import re
 while True:
     correo = input("Introduzca su correo elecrónico: ")
     valido = re.search(".+@.+\..+", correo)
-    if valido == None:
-        print("No ha introducido un correo válido, por favor inténtelo de nuevo.")
+    if valido == None: # Es decir, el correo no tiene la forma xxx@xxx.xx
+        print("No ha introducido un correo válido, por favor inténtelo de nuevo. La dirección de correo debe tener la forma xxx@xxx.xx")
     else:
         existe = re.search("\..{2,3}$", correo) # Nos aseguramos que el correo sea válido si después del punto hay dos o tres caracteres
-        if existe == None:
+        if existe == None: # Es decir, el coreo no tiene la forma xxx@xxx.xx o xxx@xxx.xxx
             print("Cuenta bloqueada a causa de un ataque.")
             exit()
         print("¡Bienvenid@!")
